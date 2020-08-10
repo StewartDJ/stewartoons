681f66431fd252337158d2aa0c65f32eae0712ac
DJ Stewart <deshastewart1992@gmail.com>
2020-08-10 14:29:54 -0700

    first commit

---
+++ js/videos.js
@@ -0,0 +1,16 @@
+console.log("javascript is linked")
+
+function selectDemoReel(){ 
+var demoReel =  '<iframe src="https://www.youtube.com/embed/XA0bq8MNdHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+document.getElementById("videoContainer").innerHTML = demoReel;
+}
+
+function selectOWKp1(){ 
+    var oWK1 =  '<iframe width="560" height="315" src="https://www.youtube.com/embed/oKsKUwwpd3I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    document.getElementById("videoContainer").innerHTML = oWK1;
+}
+
+    function selectPAP(){ 
+        var pAP =  '<iframe width="560" height="315" src="https://www.youtube.com/embed/3SPZMwlTKWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        document.getElementById("videoContainer").innerHTML = pAP;
+        }
