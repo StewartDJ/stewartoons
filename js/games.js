581306dac969a5c2259ed40c2f9ed3a382531a63
DJ Stewart <deshastewart1992@gmail.com>
2020-08-11 12:33:11 -0700

    origin master

---
+++ js/games.js
@@ -0,0 +1,4 @@
+function cssdTrailer(){ 
+    var teaser =  '<center><p class="videoContainer games-library-trailer"><iframe width="560" height="315" src="https://www.youtube.com/embed/dyVu0zqq0I0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center></p>'
+    document.getElementById("trailer").innerHTML = teaser;
+}
