6c27bd758f11b4c77ed345b4baac0787d1432340
DJ Stewart <deshastewart1992@gmail.com>
2020-08-11 20:11:09 -0700

    commit

--- js/comics.js
+++ js/comics.js
@@ -12,9 +12,36 @@ document.getElementById("comics").innerHTML = sb;
 
 
 function karateCatNumOne(){ 
-var kc =  '<img src="./assets/comics/KC1/kc.jpg"><br><img src="./assets/comics/KC1/kc1.jpg"> '
+var kc =  '<img src="./assets/comics/KC1/kc.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc1.jpg"> <br>' +
+'<img src="./assets/comics/KC1/kc2.jpg" ><br>' +
+'<img src="./assets/comics/KC1/kc3.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc4.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc5.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc6.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc7.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc8.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc9.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc10.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc11.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc12.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc13.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc14.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc15.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc16.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc17.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc18.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc19.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc20.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc21.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc22.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc23.jpg" ><br>' +
+'<img src="./assets/comics/KC1/kc24.jpg" ><br>' +
+'<img src="./assets/comics/KC1/kc25.jpg"><br>' +
+'<img src="./assets/comics/KC1/kc26.jpg">'  
 
-var kcSummary = '<h3><b>Summary:</b></h3><p> Ah yes, just what everyone came here for. Comics I wrote in middle school. You have great taste.</p>'
+var kcSummary = '<h3><b>Summary:</b>' +
+'</h3><p>Personal Rating: ★★★☆☆ </p><p> Ah yes, just what everyone came here for. Comics I wrote in middle school. You have great taste.</p>'
 
 document.getElementById("summary").innerHTML = kcSummary;
 document.getElementById("comics").innerHTML = kc;
@@ -22,9 +49,12 @@ document.getElementById("comics").innerHTML = kc;
 
 
 function redThornNumOne(){ 
-    var rt =  '<center><img src="./assets/comics/RT/00.png"></center><br> '
+    var rt =  '<center><img src="./assets/comics/RT/00.png" width="100%"></center><br> '
     
-    var rtSummary = '<h3><b>Summary:</b></h3><p> </p>'
+    var rtSummary = '<h3><b> The Red Thorn</b></h3>' +
+    '<p><b> Rating: </b> ★★★☆☆ </p>' +
+    '<p><b> Year: </b> 2011 </p>' + 
+    '<p><b>Summary: </b> </p>' 
     
     document.getElementById("summary").innerHTML = rtSummary;
     document.getElementById("comics").innerHTML = rt;
