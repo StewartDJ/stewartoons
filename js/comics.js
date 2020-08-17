7156bd95fdcdfa2c022b977c68112b4cd911dcb0
DJ Stewart <deshastewart1992@gmail.com>
2020-08-17 09:01:30 -0700

    new pages

--- js/comics.js
+++ js/comics.js
@@ -10,7 +10,6 @@ document.getElementById("summary").innerHTML = sbSummary;
 document.getElementById("comics").innerHTML = sb;
 }
 
-
 function karateCatNumOne(){ 
 var kc =  '<img src="./assets/comics/KC1/kc.jpg"><br>' +
 '<img src="./assets/comics/KC1/kc1.jpg"> <br>' +
@@ -48,6 +47,7 @@ document.getElementById("comics").innerHTML = kc;
 }
 
 
+
 function redThornNumOne(){ 
     var rt =  '<center><img src="./assets/comics/RT/00.png" width="100%"></center><br> '
     
@@ -59,4 +59,226 @@ function redThornNumOne(){
     document.getElementById("summary").innerHTML = rtSummary;
     document.getElementById("comics").innerHTML = rt;
     }
+
+    function karateCatNumTwo(){ 
+        var kc2 =  '<img src="./assets/comics/KC2/kc.jpg"><br>' +
+        
+        '<img src="./assets/comics/KC2/kc0.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc1.jpg"> <br>' +
+        '<img src="./assets/comics/KC2/kc2.jpg" ><br>' +
+        '<img src="./assets/comics/KC2/kc3.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc4.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc5.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc6.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc7.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc8.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc9.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc10.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc11.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc12.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc13.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc14.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc15.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc16.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc17.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc18.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc19.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc20.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc21.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc22.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc23.jpg" ><br>' +
+        '<img src="./assets/comics/KC2/kc24.jpg" ><br>' +
+        '<img src="./assets/comics/KC2/kc25.jpg"><br>' +
+        '<img src="./assets/comics/KC2/kc26.jpg">' +   
+        '<img src="./assets/comics/KC2/kc27.jpg">' + 
+        '<img src="./assets/comics/KC2/kc28.jpg">' +
+        '<img src="./assets/comics/KC2/kc29.jpg">'  
+        
+        var kcSummary2 = '<h3><b>Summary:</b>' +
+        '</h3><p>Personal Rating: ★★★☆☆ </p><p> Volume 2 </p>'
+        
+        document.getElementById("summary").innerHTML = kcSummary2;
+        document.getElementById("comics").innerHTML = kc2;
+        }
+        
     
+        function karateCatNumThree(){ 
+            var kc3 =  '<img src="./assets/comics/KC3/kc.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc1.jpg"> <br>' +
+            '<img src="./assets/comics/KC3/kc2.jpg" ><br>' +
+            '<img src="./assets/comics/KC3/kc3.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc4.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc5.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc6.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc7.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc8.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc9.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc10.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc11.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc12.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc13.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc14.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc15.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc16.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc17.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc18.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc19.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc20.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc21.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc22.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc23.jpg" ><br>' +
+            '<img src="./assets/comics/KC3/kc24.jpg" ><br>' +
+            '<img src="./assets/comics/KC3/kc25.jpg"><br>' +
+            '<img src="./assets/comics/KC3/kc26.jpg">' +   
+            '<img src="./assets/comics/KC3/kc27.jpg">' + 
+            '<img src="./assets/comics/KC3/kc28.jpg">' +
+            '<img src="./assets/comics/KC3/kc29.jpg">'  
+            
+            var kcSummary3 = '<h3><b>Summary:</b>' +
+            '</h3><p>Personal Rating: ★★★☆☆ </p><p> Volume 3 </p>'
+            
+            document.getElementById("summary").innerHTML = kcSummary3;
+            document.getElementById("comics").innerHTML = kc3;
+            }
+            
+
+            function karateCatNumFour(){ 
+                var kc4 =  '<img src="./assets/comics/KC4/kc.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc0.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc1.jpg"> <br>' +
+                '<img src="./assets/comics/KC4/kc2.jpg" ><br>' +
+                '<img src="./assets/comics/KC4/kc3.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc4.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc5.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc6.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc7.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc8.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc9.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc10.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc11.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc12.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc13.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc14.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc15.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc16.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc17.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc18.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc19.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc20.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc21.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc22.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc23.jpg" ><br>' +
+                '<img src="./assets/comics/KC4/kc24.jpg" ><br>' +
+                '<img src="./assets/comics/KC4/kc25.jpg"><br>' +
+                '<img src="./assets/comics/KC4/kc26.jpg">'  
+                
+                var kcSummary4 = '<h3><b>Summary:</b>' +
+                '</h3><p>Personal Rating: ★★★☆☆ </p><p> Volume 3 </p>'
+                
+                document.getElementById("summary").innerHTML = kcSummary4;
+                document.getElementById("comics").innerHTML = kc4;
+                }
+                
+                function karateCatNumFive(){ 
+                    console.log('am clicked')
+                    var kc5 =  '<img src="./assets/comics/KC5/kc.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc0.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc1.jpg"> <br>' +
+                    '<img src="./assets/comics/KC5/kc2.jpg" ><br>' +
+                    '<img src="./assets/comics/KC5/kc3.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc4.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc5.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc6.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc7.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc8.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc9.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc10.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc11.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc12.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc13.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc14.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc15.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc16.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc17.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc18.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc19.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc20.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc21.jpg"><br>' +
+                    '<img src="./assets/comics/KC5/kc22.jpg"><br>'  
+                    
+                    var kcSummary5 = '<h3><b>Summary:</b>' +
+                    '</h3><p>Personal Rating: ★★★☆☆ </p><p> Volume 3 </p>'
+                    
+                    document.getElementById("summary").innerHTML = kcSummary5;
+                    document.getElementById("comics").innerHTML = kc5;
+                    }
+                    
+                    function karateCatNumSix(){ 
+                        var kc6 =  '<img src="./assets/comics/KC6/kc.jpg"><br>' +
+                        
+                        '<img src="./assets/comics/KC6/kc1.jpg"> <br>' +
+                        '<img src="./assets/comics/KC6/kc2.jpg" ><br>' +
+                        '<img src="./assets/comics/KC6/kc3.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc4.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc5.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc6.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc7.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc8.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc9.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc10.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc11.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc12.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc13.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc14.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc15.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc16.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc17.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc18.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc19.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc20.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc21.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc22.jpg"><br>'  +
+                        '<img src="./assets/comics/KC6/kc23.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc24.jpg"><br>' +
+                        '<img src="./assets/comics/KC6/kc25.jpg"><br>' 
+                        
+                        var kcSummary6 = '<h3><b>Summary:</b>' +
+                        '</h3><p>Personal Rating: ★★★☆☆ </p><p> Volume 3 </p>'
+                        
+                        document.getElementById("summary").innerHTML = kcSummary6;
+                        document.getElementById("comics").innerHTML = kc6;
+                        }
+                        
+                        function karateCatNumSeven(){ 
+                            var kc7 =  '<img src="./assets/comics/KC7/kc.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc0.jpg"> <br>' +
+                            '<img src="./assets/comics/KC7/kc1.jpg"> <br>' +
+                            '<img src="./assets/comics/KC7/kc2.jpg" ><br>' +
+                            '<img src="./assets/comics/KC7/kc3.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc4.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc5.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc6.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc7.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc8.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc9.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc10.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc11.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc12.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc13.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc14.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc15.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc16.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc17.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc18.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc19.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc20.jpg"><br>' +
+                            '<img src="./assets/comics/KC7/kc21.jpg"><br>' 
+                            
+                            var kcSummary7 = '<h3><b>Summary:</b>' +
+                            '</h3><p>Personal Rating: ★★★☆☆ </p><p> Volume 3 </p>'
+                            
+                            document.getElementById("summary").innerHTML = kcSummary7;
+                            document.getElementById("comics").innerHTML = kc7;
+                            }
+                            
+            
